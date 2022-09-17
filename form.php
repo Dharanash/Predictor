f125d1ca8d56b989ab99ebe6f882b3d52380ffa6
Dharanash <20tuec018@skct.edu.in>
2022-09-17 15:38:05 +0530

    I stored all the input to my local database.

--- form.php
+++ form.php
@@ -1,3 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>form</title>
+  
+  
+</head>
+<body>
+  <img src="R.jpg" alt="Image" >
+  <style>
+    body img{
+      width: 100%;
+      height: 780px;
+      background-size:100%;
+      display: block;
+      ali
+    }
+  </style>
 <?php
 
 $name= $_POST['name'];
@@ -5,40 +26,22 @@ $color= $_POST['color'];
 $Date= $_POST['Date'];
 $gender= $_POST['gender'];
 
-if(!empty($name) || !empty($color) || !empty($Date) || !empty($gender)){
-
-    $host='localhost';
-    $dbname='project-predictor';
-
-    // Create connection
-$conn = new mysqli ($host, $dbname);
-
-if (mysqli_connect_error()){
-  die('Connect Error ('. mysqli_connect_errno() .') '
-    . mysqli_connect_error());
+// Database connection
+$conn = new mysqli('localhost','root','','predictor');
+if($conn->connect_error){
+  echo "$conn->connect_error";
+  die("Connection Failed : ". $conn->connect_error);
+} else {
+  $stmt = $conn->prepare("insert into form(name, color, Date, gender) values(?, ?, ?, ?)");
+  $stmt->bind_param("ssss", $name, $color, $Date, $gender);
+  $execval = $stmt->execute();
+  $stmt->close();
+  $conn->close();
 }
-else{
-   
-    $INSERT = "INSERT Into form (name , color ,Date, gender )values(?,?,?,?)";
-  
-  
-       //checking username
-        
-        $stmt->close();
-        $stmt = $conn->prepare($INSERT);
-        $stmt->bind_param("stds", $name,$color,$Date,$gender);
-        $stmt->execute();
-        echo "New record inserted sucessfully";
-}
-       $stmt->close();
-       $conn->close();
-      }
-   else {
-   echo "All field are required";
-   die();
-}
-
-
-
 
 ?>
+
+</body>
+</html>
+
+
